f2564bd47e4bd6a56f073e6e1154040af0e60da0
Lawrence Maceren <lawrencemaceren96@gmail.com>
2017-01-29 13:32:28 -0500

    Added getNeighbors() and cursor
    Also included the 5 maps + 10 endpoints for each map

--- grid.py
+++ grid.py
@@ -20,6 +20,9 @@ start_y = 0
 goal_x = 1
 goal_y = 1
 
+cursor_x = 0
+cursor_y = 0
+
 # Make Random Grid
 def makeGrid():
 	# Make 8 hard to traverse areas
@@ -207,11 +210,50 @@ def drawScreen():
 	pygame.draw.circle(GameScreen, (255,0,0), (start_x*blockwidth+blockwidth/2+10,start_y*blockwidth+blockwidth/2+10),blockwidth/2, 0)
 	pygame.draw.circle(GameScreen, (0,255,0), (goal_x*blockwidth+blockwidth/2+10,goal_y*blockwidth+blockwidth/2+10),blockwidth/2, 0)
 	
+	pygame.draw.rect(GameScreen, (255,0,0), (cursor_x*blockwidth+9,cursor_y*blockwidth+9,blockwidth+2,blockwidth+2), 2)
+	
 	# Draw text
 	label = myfont.render("G = Generate Grid     E = New Start (R) and Goal (G)     S = Save Map     L = Load Map     Esc = Quit", 1, (0,0,0))
 	GameScreen.blit(label, (30, blockwidth*GridRows+14))
 	
+	label = myfont.render("("+str(cursor_x)+","+str(cursor_y)+")", 1, (0,0,0))
+	GameScreen.blit(label, (10+blockwidth*GridCols+30, 30))
+	
+	label = myfont.render("Type: "+grid[cursor_x][cursor_y], 1, (0,0,0))
+	GameScreen.blit(label, (10+blockwidth*GridCols+30, 60))
+	
+	label = myfont.render("Neighbors:", 1, (0,0,0))
+	GameScreen.blit(label, (10+blockwidth*GridCols+30, 120))
+	
+	draw_y = 142
+	neighbors = getNeighbors(cursor_x,cursor_y)
+	
+	for neighbor in neighbors:
+		label = myfont.render("("+str(neighbor[0])+","+str(neighbor[1])+")", 1, (0,0,0))
+		GameScreen.blit(label, (10+blockwidth*GridCols+30, draw_y))
+		draw_y += 22
+	
 	pygame.display.update()
+
+# Get neighbors of a cell
+def getNeighbors(x,y):
+	myneighbors = [(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)]
+	
+	'''for neighbor in myneighbors:
+		if neighbor[0] < 0:							# Out of bounds left
+			myneighbors.remove(neighbor)
+		elif neighbor[0] >= GridCols:				# Out of bounds right
+			myneighbors.remove(neighbor)
+		elif neighbor[1] < 0:						# Out of bounds up
+			myneighbors.remove(neighbor)
+		elif neighbor[1] >= GridRows:				# Out of bounds down
+			myneighbors.remove(neighbor)
+		elif grid[neighbor[0]][neighbor[1]] == '0':	# Wall
+			myneighbors.remove(neighbor)'''
+			
+	myneighbors[:] = [neighbor for neighbor in myneighbors if neighbor[0]>=0 and neighbor[1]>=0 and neighbor[0]<GridCols and neighbor[1]<GridRows and grid[neighbor[0]][neighbor[1]] != '0']
+	
+	return myneighbors
 	
 # Main loop
 running = True
@@ -223,16 +265,18 @@ while(running):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-		if event.type == pygame.KEYDOWN:
+		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				running = False
-			if event.key == pygame.K_g:
+			elif event.key == pygame.K_g:
 				grid = [['1' for y in range(GridRows)] for x in range(GridCols)]
 				areacoordinates = makeGrid()
 				start_x,start_y,goal_x,goal_y = generateStartFinish()
-			if event.key == pygame.K_e:
+				print "Generated new map"
+			elif event.key == pygame.K_e:
 				start_x,start_y,goal_x,goal_y = generateStartFinish()
-			if event.key == pygame.K_s:
+				print "Generated new start and finish points"
+			elif event.key == pygame.K_s:
 				# Save map: get filename
 				filename = raw_input("Save map to: ")
 				with open(filename,"w") as mapfile:
@@ -252,7 +296,7 @@ while(running):
 					
 					mapfile.close()
 				print "Map saved!"
-			if event.key == pygame.K_l:
+			elif event.key == pygame.K_l:
 				# Load map: get filename
 				filename = raw_input("Load map from: ")
 				with open(filename,"r") as mapfile:
@@ -273,8 +317,19 @@ while(running):
 					
 					mapfile.close()
 				print "Map loaded!"
-	
-	# Draw grid
-	drawScreen()
+			elif event.key == pygame.K_UP:
+				if cursor_y-1 >= 0:
+					cursor_y -= 1
+			elif event.key == pygame.K_LEFT:
+				if cursor_x-1 >= 0:
+					cursor_x -= 1
+			elif event.key == pygame.K_RIGHT:
+				if cursor_x+1 < GridCols:
+					cursor_x += 1
+			elif event.key == pygame.K_DOWN:
+				if cursor_y+1 < GridRows:
+					cursor_y += 1
+		# Draw grid
+		drawScreen()
 
 pygame.quit()
